f7a13899dd47243c91f0a316bca952783e2951d0
RetroPipes <eric.ahnell@puttysoftware.com>
2025-08-24 16:24:36 -0400

    Add Image property to DungeonObject and associated test.

--- DungeonDiver.Client/DungeonObject.cs
+++ DungeonDiver.Client/DungeonObject.cs
@@ -1,10 +1,21 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace DungeonDiver.Client;
 
 public class DungeonObject
 {
+    [JsonConstructor]
     public DungeonObject(int id) => Id = id;
 
+    public DungeonObject(int id, string image)
+    {
+        Id = id;
+        Image = image;
+    }
+
     public int Id { get; init; }
+
+    [JsonIgnore]
+    public string? Image { get; init; }
 }
--- DungeonDiver.Tests/DungeonObjectTests.cs
+++ DungeonDiver.Tests/DungeonObjectTests.cs
@@ -1,4 +1,5 @@
-﻿using DungeonDiver.Client;
+﻿using System.Text.Json;
+using DungeonDiver.Client;
 
 namespace DungeonDiver.Tests;
 
@@ -9,5 +10,17 @@ public class DungeonObjectTests
     {
         var dungeonObject = new DungeonObject(id: 1);
         Assert.Equal(1, dungeonObject.Id);
+    }
+
+    [Fact]
+    public void Create_WithId1_AndImage()
+    {
+        var dungeonObject = new DungeonObject(id: 1, image: "tile.png");
+        Assert.Equal(1, dungeonObject.Id);
+        Assert.Equal("tile.png", dungeonObject.Image);
+        var dungeonObjectJson = JsonSerializer.Serialize(dungeonObject);
+        var recreatedDungeonObject = JsonSerializer.Deserialize<DungeonObject>(dungeonObjectJson)!;
+        // Serialization does NOT persist the Image property
+        Assert.Null(recreatedDungeonObject.Image);
     }
 }
