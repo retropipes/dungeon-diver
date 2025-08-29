a74ebe32e74e38914f4178a31fcb8ed5d1af5c8d
RetroPipes <eric.ahnell@puttysoftware.com>
2025-08-29 09:41:50 -0400

    Dungeon Object Tests: Add Solid property.

--- DungeonDiver.Tests/DungeonObjectTests.cs
+++ DungeonDiver.Tests/DungeonObjectTests.cs
@@ -23,4 +23,20 @@ public class DungeonObjectTests
         // Serialization does NOT persist the Image property
         Assert.Null(recreatedDungeonObject.Image);
     }
+
+    [Fact]
+    public void Create_WithId1_AndSolid()
+    {
+        var dungeonObject = new DungeonObject(id: 1, solid: true);
+        Assert.Equal(1, dungeonObject.Id);
+        Assert.True(dungeonObject.Solid);
+    }
+
+    [Fact]
+    public void Create_WithId1_AndNotSolid()
+    {
+        var dungeonObject = new DungeonObject(id: 1, solid: false);
+        Assert.Equal(1, dungeonObject.Id);
+        Assert.False(dungeonObject.Solid);
+    }
 }
