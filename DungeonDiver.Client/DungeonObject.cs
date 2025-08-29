3619063dd573e75c66fbf978aefdbc4a9eb0867a
RetroPipes <eric.ahnell@puttysoftware.com>
2025-08-29 09:44:26 -0400

    Implement Solid property. Tests pass now.

--- DungeonDiver.Client/DungeonObject.cs
+++ DungeonDiver.Client/DungeonObject.cs
@@ -5,16 +5,23 @@ namespace DungeonDiver.Client;
 public class DungeonObject
 {
     [JsonConstructor]
-    public DungeonObject(int id) => Id = id;
+    public DungeonObject(int id, bool solid)
+    {
+        Id = id;
+        Solid = solid;
+    }
 
-    public DungeonObject(int id, string image)
+    public DungeonObject(int id, string image, bool solid)
     {
         Id = id;
         Image = image;
+        Solid = solid;
     }
 
     public int Id { get; init; }
 
     [JsonIgnore]
     public string? Image { get; init; }
+
+    public bool Solid { get; init; }
 }
--- DungeonDiver.Tests/DungeonObjectTests.cs
+++ DungeonDiver.Tests/DungeonObjectTests.cs
@@ -8,28 +8,21 @@ public class DungeonObjectTests
     [Fact]
     public void Create_WithId1()
     {
-        var dungeonObject = new DungeonObject(id: 1);
+        var dungeonObject = new DungeonObject(id: 1, solid: true);
         Assert.Equal(1, dungeonObject.Id);
+        Assert.True(dungeonObject.Solid);
     }
 
     [Fact]
     public void Create_WithId1_AndImage()
     {
-        var dungeonObject = new DungeonObject(id: 1, image: "tile.png");
+        var dungeonObject = new DungeonObject(id: 1, image: "tile.png", solid: false);
         Assert.Equal(1, dungeonObject.Id);
         Assert.Equal("tile.png", dungeonObject.Image);
         var dungeonObjectJson = JsonSerializer.Serialize(dungeonObject);
         var recreatedDungeonObject = JsonSerializer.Deserialize<DungeonObject>(dungeonObjectJson)!;
         // Serialization does NOT persist the Image property
         Assert.Null(recreatedDungeonObject.Image);
-    }
-
-    [Fact]
-    public void Create_WithId1_AndSolid()
-    {
-        var dungeonObject = new DungeonObject(id: 1, solid: true);
-        Assert.Equal(1, dungeonObject.Id);
-        Assert.True(dungeonObject.Solid);
     }
 
     [Fact]
